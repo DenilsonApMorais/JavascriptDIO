fe9eba5a19353039b67cc27980232604aaef35b6
Denilson Morais <denilsonapmorais@gmail.com>
2024-02-05 18:05:07 -0300

    fist commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const lista  = [1, 2,3, 4, 5, 6, 7, 8, 9, 10]  // Criando uma lista
+// valor indice e referencia para lista 
+lista.forEach((value, i, listRef) => {
+    console.log(value, i , listRef)
+})
